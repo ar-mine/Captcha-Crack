37301654c0af9a0cf4ed63df54ad6a9b0be72887
ar-mine <934218777@qq.com>
2020-07-14 19:35:48 +0800

    emmm
    Nothing new

--- SSD.py
+++ SSD.py
@@ -1,11 +1,313 @@
-from mxnet import autograd, contrib, gluon, image, init, np, npx
+from mxnet import autograd, contrib, gluon, image, init, np, npx, ndarray as nd
 from mxnet.gluon import nn
+import mxnet as mx
+import os
+import matplotlib.pyplot as plt
+import time
 
 npx.set_np()
+
+
+def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
+    """Plot a list of images."""
+    figsize = (num_cols * scale, num_rows * scale)
+    _, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = axes.flatten()
+    for i, (ax, img) in enumerate(zip(axes, imgs)):
+        ax.imshow(img.asnumpy())
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+        if titles:
+            ax.set_title(titles[i])
+    return axes
+
+
+def bbox_to_rect(bbox, color):
+    """Convert bounding box to matplotlib format."""
+    # Convert the bounding box (top-left x, top-left y, bottom-right x,
+    # bottom-right y) format to matplotlib format: ((upper-left x,
+    # upper-left y), width, height)
+    return plt.Rectangle(xy=(bbox[0], bbox[1]), width=bbox[2]-bbox[0], height=bbox[3]-bbox[1],
+                        fill=False, edgecolor=color, linewidth=2)
+
+
+def show_bboxes(axes, bboxes, labels=None, colors=None):
+    """Show bounding boxes."""
+    def _make_list(obj, default_values=None):
+        if obj is None:
+            obj = default_values
+        elif not isinstance(obj, (list, tuple)):
+            obj = [obj]
+        return obj
+    labels = _make_list(labels)
+    colors = _make_list(colors, ['b', 'g', 'r', 'm', 'c'])
+    for i, bbox in enumerate(bboxes):
+        color = colors[i % len(colors)]
+        rect = bbox_to_rect(bbox.asnumpy(), color)
+        axes.add_patch(rect)
+        if labels and len(labels) > i:
+            text_color = 'k' if color == 'w' else 'w'
+            axes.text(rect.xy[0], rect.xy[1], labels[i],
+                        va='center', ha='center', fontsize=9, color=text_color,
+                        bbox=dict(facecolor=color, lw=0))
+
+
+def load_data_pikachu(batch_size, edge_size=256):
+    """Load the pikachu dataset"""
+    data_dir = './asset/dataset'
+    train_iter = image.ImageDetIter(
+        path_imgrec=os.path.join(data_dir, 'train.rec'),
+        path_imgidx=os.path.join(data_dir, 'train.idx'),
+        batch_size=batch_size,
+        data_shape=(3, edge_size, edge_size),  # The shape of the output image
+        shuffle=True,   # Read the dataset in random order
+        rand_crop=1,    # The probability of random cropping is 1
+        min_object_covered=0.95, max_attempts=200)
+    val_iter = image.ImageDetIter(
+        path_imgrec=os.path.join(data_dir, 'val.rec'),
+        batch_size=batch_size,
+        data_shape=(3, edge_size, edge_size),
+        shuffle=False)
+    return train_iter, val_iter
+
+
+batch_size, edge_size = 32, 256
+train_iter, _ = load_data_pikachu(batch_size, edge_size)
+batch = train_iter.next()
+# print(batch.data[0].shape, batch.label[0].shape)
+
+imgs = (batch.data[0][0:10].transpose(0, 2, 3, 1)) / 255
+axes = show_images(imgs, 2, 5, scale=2)
+for ax, label in zip(axes, batch.label[0][0:10]):
+    show_bboxes(ax, [label[0][1:5] * edge_size], colors=['w'])
+
+# plt.show()
 
 
 def cls_predictor(num_anchors, num_classes):
     return nn.Conv2D(num_anchors * (num_classes + 1), kernel_size=3, padding=1)
 
 
-print("test")
+def bbox_predictor(num_anchors):
+    return nn.Conv2D(num_anchors * 4, kernel_size=3, padding=1)
+
+
+def forward(x, block):
+    block.initialize()
+    return block(x)
+
+
+Y1 = forward(np.zeros((2, 8, 20, 20)), cls_predictor(5, 10))
+Y2 = forward(np.zeros((2, 16, 10, 10)), cls_predictor(3, 10))
+print(Y1.shape, Y2.shape)
+
+def flatten_pred(pred):
+    return npx.batch_flatten(pred.transpose(0, 2, 3, 1))
+
+
+def concat_preds(preds):
+    return np.concatenate([flatten_pred(p) for p in preds], axis=1)
+
+
+def down_sample_blk(num_channels):
+    blk = nn.Sequential()
+    for _ in range(2):
+        blk.add(nn.Conv2D(num_channels, kernel_size=3, padding=1),
+                nn.BatchNorm(in_channels=num_channels),
+                nn.Activation('relu'))
+    blk.add(nn.MaxPool2D(2))
+    return blk
+
+
+def base_net():
+    blk = nn.Sequential()
+    for num_filters in [16, 32, 64]:
+        blk.add(down_sample_blk(num_filters))
+    return blk
+
+
+def get_blk(i):
+    if i == 0:
+        blk = base_net()
+    elif i == 4:
+        blk = nn.GlobalMaxPool2D()
+    else:
+        blk = down_sample_blk(128)
+    return blk
+
+
+def blk_forward(X, blk, size, ratio, cls_predictor, bbox_predictor):
+    Y = blk(X)
+    anchors = npx.multibox_prior(Y, sizes=size, ratios=ratio)
+    cls_preds = cls_predictor(Y)
+    bbox_preds = bbox_predictor(Y)
+    return (Y, anchors, cls_preds, bbox_preds)
+
+
+sizes = [[0.2, 0.272], [0.37, 0.447], [0.54, 0.619], [0.71, 0.79], [0.88, 0.961]]
+ratios = [[1, 2, 0.5]] * 5
+num_anchors = len(sizes[0]) + len(ratios[0]) - 1
+
+
+class TinySSD(nn.Block):
+    def __init__(self, num_classes, **kwargs):
+        super(TinySSD, self).__init__(**kwargs)
+        self.num_classes = num_classes
+        for i in range(5):
+            # The assignment statement is self.blk_i = get_blk(i)
+            setattr(self, 'blk_%d' % i, get_blk(i))
+            setattr(self, 'cls_%d' % i, cls_predictor(num_anchors,
+            num_classes))
+            setattr(self, 'bbox_%d' % i, bbox_predictor(num_anchors))
+    def forward(self, X):
+        anchors, cls_preds, bbox_preds = [None] * 5, [None] * 5, [None] * 5
+        for i in range(5):
+            # getattr(self, 'blk_%d' % i) accesses self.blk_i
+            X, anchors[i], cls_preds[i], bbox_preds[i] = blk_forward(
+            X, getattr(self, 'blk_%d' % i), sizes[i], ratios[i],
+            getattr(self, 'cls_%d' % i), getattr(self, 'bbox_%d' % i))
+        # In the reshape function, 0 indicates that the batch size remains
+        # unchanged
+        anchors = np.concatenate(anchors, axis=1)
+        cls_preds = concat_preds(cls_preds)
+        cls_preds = cls_preds.reshape(
+        cls_preds.shape[0], -1, self.num_classes + 1)
+        bbox_preds = concat_preds(bbox_preds)
+        return anchors, cls_preds, bbox_preds
+
+def try_all_gpus():
+    """Return all available GPUs, or [cpu(),] if no GPU exists."""
+    ctxes = [npx.gpu(i) for i in range(npx.num_gpus())]
+    return ctxes if ctxes else [npx.cpu()]
+
+
+net = TinySSD(num_classes=1)
+net.initialize()
+X = np.zeros((32, 3, 256, 256))
+anchors, cls_preds, bbox_preds = net(X)
+batch_size = 8
+train_iter, _ = load_data_pikachu(batch_size)
+
+ctx, net = try_all_gpus(), TinySSD(num_classes=1)
+net.initialize(init=init.Xavier(), ctx=ctx)
+trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': 0.2, 'wd': 5e-4})
+
+cls_loss = gluon.loss.SoftmaxCrossEntropyLoss()
+bbox_loss = gluon.loss.L1Loss()
+
+
+def calc_loss(cls_preds, cls_labels, bbox_preds, bbox_labels, bbox_masks):
+    cls = cls_loss(cls_preds, cls_labels)
+    bbox = bbox_loss(bbox_preds * bbox_masks, bbox_labels * bbox_masks)
+    return cls + bbox
+
+
+def cls_eval(cls_preds, cls_labels):
+    # Because the category prediction results are placed in the final
+    # dimension, argmax must specify this dimension
+    return float((cls_preds.argmax(axis=-1) == cls_labels).sum())
+
+
+def bbox_eval(bbox_preds, bbox_labels, bbox_masks):
+    return float((np.abs((bbox_labels - bbox_preds) * bbox_masks)).sum())
+
+
+class Timer(object):
+    """Record multiple running times."""
+    def __init__(self):
+        self.times = []
+        self.start()
+    def start(self):
+        # Start the timer
+        self.start_time = time.time()
+    def stop(self):
+        # Stop the timer and record the time in a list
+        self.times.append(time.time() - self.start_time)
+        return self.times[-1]
+    def avg(self):
+        # Return the average time
+        return sum(self.times)/len(self.times)
+    def sum(self):
+        # Return the sum of time
+        return sum(self.times)
+    def cumsum(self):
+        # Return the accumuated times
+        return np.array(self.times).cumsum().tolist()
+
+class Accumulator(object):
+    """Sum a list of numbers over time"""
+    def __init__(self, n):
+        self.data = [0.0] * n
+    def add(self, *args):
+        self.data = [a+b for a, b in zip(self.data, args)]
+    def reset(self):
+        self.data = [0] * len(self.data)
+    def __getitem__(self, i):
+        return self.data[i]
+
+
+
+num_epochs, timer = 20, Timer()
+# animator = d2l.Animator(xlabel='epoch', xlim=[1, num_epochs], legend=['class error', 'bbox mae'])
+for epoch in range(num_epochs):
+    # accuracy_sum, mae_sum, num_examples, num_labels
+    metric = Accumulator(4)
+    train_iter.reset() # Read data from the start.
+    for batch in train_iter:
+        timer.start()
+        X = batch.data[0].as_in_context(ctx)
+        Y = batch.label[0].as_in_context(ctx)
+        with autograd.record():
+            # Generate multiscale anchor boxes and predict the category and
+            # offset of each
+            anchors, cls_preds, bbox_preds = net(X)
+            # Label the category and offset of each anchor box
+            bbox_labels, bbox_masks, cls_labels = npx.multibox_target(
+            anchors, Y, cls_preds.transpose(0, 2, 1))
+            # Calculate the loss function using the predicted and labeled
+            # category and offset values
+            l = calc_loss(cls_preds, cls_labels, bbox_preds, bbox_labels, bbox_masks)
+
+        l.backward()
+        trainer.step(batch_size)
+        metric.add(cls_eval(cls_preds, cls_labels), cls_labels.size,
+                    bbox_eval(bbox_preds, bbox_labels, bbox_masks),
+                    bbox_labels.size)
+    cls_err, bbox_mae = 1-metric[0]/metric[1], metric[2]/metric[3]
+    # animator.add(epoch+1, (cls_err, bbox_mae))
+print('class err %.2e, bbox mae %.2e' % (cls_err, bbox_mae))
+print('%.1f exampes/sec on %s' % (train_iter.num_image/timer.stop(), ctx))
+# plt.show()
+
+img = image.imread('../img/pikachu.jpg')
+feature = image.imresize(img, 256, 256).astype('float32')
+X = np.expand_dims(feature.transpose(2, 0, 1), axis=0)
+
+def predict(X):
+    anchors, cls_preds, bbox_preds = net(X.as_in_context(ctx))
+    cls_probs = npx.softmax(cls_preds).transpose(0, 2, 1)
+    output = npx.multibox_detection(cls_probs, bbox_preds, anchors)
+    idx = [i for i, row in enumerate(output[0]) if row[0] != -1]
+    return output[0, idx]
+
+def use_svg_display():
+    """Use the svg format to display a plot in Jupyter."""
+    display.set_matplotlib_formats('svg')
+
+def set_figsize(figsize=(3.5, 2.5)):
+    """Set the figure size for matplotlib."""
+    use_svg_display()
+    plt.rcParams['figure.figsize'] = figsize
+
+def display(img, output, threshold):
+    set_figsize((5, 5))
+    fig = plt.imshow(img.asnumpy())
+    for row in output:
+        score = float(row[1])
+        if score < threshold:
+            continue
+        h, w = img.shape[0:2]
+        bbox = [row[2:6] * np.array((w, h, w, h), ctx=row.context)]
+        show_bboxes(fig.axes, bbox, '%.2f' % score, 'w')
+        display(img, output, threshold=0.3)
+output = predict(X)
