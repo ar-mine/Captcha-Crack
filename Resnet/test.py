2302870289d6b9182f7c3e662a33bde2da627d35
ar-mine <934218777@qq.com>
2020-07-27 19:12:06 +0800

    Revert "Revert "Update -t v1.0""
    This reverts commit bf916b06dfc2245983d81b117061d33c56ca6581.

--- Resnet/test.py
+++ Resnet/test.py
@@ -8,13 +8,13 @@ from tqdm import tqdm
 import net
 
 
-DATASET_ROOT = "F:/Dataset/Captcha/"
+DATASET_ROOT = "E:/Dataset/Captcha/"
 IMAGE_DIR = os.path.join(DATASET_ROOT, 'img')
 LABEL_DIR = os.path.join(DATASET_ROOT, 'rec')
 DEFAULT_FONTS = [os.path.join('./asset/font', 'simhei.ttf')]
 GEN_NUM = 25000
 NUM_CLASSES = 10
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 DICTSET = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
            'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u',
@@ -47,8 +47,8 @@ transform_test = data.vision.transforms.Compose([am.g_totensor, am.g_normalize])
 train_iter = data.DataLoader(train_set.transform_first(transform_test), BATCH_SIZE, shuffle=True, last_batch='keep')
 
 cnn = net.CNN(NUM_CLASSES)
-loss = gluon.loss.L2Loss()
-ctx, num_epochs, lr, wd = am.try_gpu(), 1, 0.001, 5e-4
+loss = gluon.loss.SoftmaxCrossEntropyLoss()
+ctx, num_epochs, lr, wd = am.try_gpu(), 15, 0.005, 5e-4
 lr_period, lr_decay, net = 80, 0.1, cnn.get_net()
 net.initialize(ctx=ctx, init=init.Xavier())
 trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': lr, 'momentum': 0.9, 'wd': wd})
@@ -58,10 +58,10 @@ for epoch in range(num_epochs):
     train_l_sum, train_acc_sum, n = 0.0, 0.0, 0
     for X, y in train_iter:
         X = X.as_in_context(ctx)
-        y = am.encoder(y, 10, c_len=4, ctx=ctx).as_in_context(ctx)
+        y = am.encoder(y, NUM_CLASSES, c_len=4, ctx=ctx).as_in_context(ctx)
         with autograd.record():
             y_hat = net(X)
-            l = loss(y_hat, y).sum()
+            l = am.decode_loss(y, y_hat, NUM_CLASSES, 4, loss)
         l.backward()
         trainer.step(BATCH_SIZE)
         train_l_sum += float(l)
@@ -73,15 +73,15 @@ for epoch in range(num_epochs):
     print(epoch)
 
 
-src = image.imread("F:/Dataset/Captcha/img/0.png").as_in_context(ctx)
+src = image.imread("E:/Dataset/Captcha/img/4.png").as_in_context(ctx)
 X = am.g_totensor(src)
 X = am.g_normalize(X)
 X = np.expand_dims(X, axis=0)
 Y = net(X)
-print(Y[0, 0:10])
-print(Y[0, 10:20])
-print(Y[0, 20:30])
-print(Y[0, 30:40])
+print(Y[0, 0:10].argmax())
+print(Y[0, 10:20].argmax())
+print(Y[0, 20:30].argmax())
+print(Y[0, 30:40].argmax())
 
 
 
